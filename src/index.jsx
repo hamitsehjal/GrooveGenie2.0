dd9ebaa6c4bf60d7e5870118fb50b10c3d2ca907
hamitsehjal <hamitsehjal26@gmail.com>
2023-04-19 12:39:58 -0400

    Made redux store available to all the components in the application

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Browser } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Browser>
-      <App />
-    </Browser>
+    <Provider store={store}>
+      <Browser>
+        <App />
+      </Browser>
+    </Provider>
   </React.StrictMode>,
 )
